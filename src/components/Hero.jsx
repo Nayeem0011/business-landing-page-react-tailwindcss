dfa9bc732542033b9dcda92f84cedf5e56f65cea
Nayeem0011 <sknayeemislam384@gmail.com>
2025-10-05 00:39:15 +0600

    smooth scroll added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import heroImage from "../assets/hero-image.png";
 const Hero = () => {
   return (
     <div>
-      <section className="flex flex-col md:flex-row justify-between items-center px-4 sm:px-6 lg:px-8 pt-44 pb-16 container mx-auto">
+      <section id="home" className="flex flex-col md:flex-row justify-between items-center px-4 sm:px-6 lg:px-8 pt-44 pb-16 container mx-auto">
         {/* Left col */}
         <div className="w-full md:w-1/2 space-y-8">
           {/* star badge */}
